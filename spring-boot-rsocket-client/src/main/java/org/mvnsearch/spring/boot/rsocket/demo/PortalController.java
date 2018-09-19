bd38934560c3f6fce052bb9967f79795dc3126e8
linux_china <libing.chen@gmail.com>
2018-09-18 20:58:20 -0700

    portal demo for test

---
+++ spring-boot-rsocket-client/src/main/java/org/mvnsearch/spring/boot/rsocket/demo/PortalController.java
@@ -0,0 +1,49 @@
+package org.mvnsearch.spring.boot.rsocket.demo;
+
+import io.rsocket.RSocket;
+import org.mvnsearch.user.User;
+import org.mvnsearch.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+/**
+ * portal demo for test
+ *
+ * @author linux_china
+ */
+@RestController
+public class PortalController {
+    @Autowired
+    RSocket rSocket;
+    @Autowired
+    UserService userService;
+
+    @RequestMapping("/user")
+    public Mono<User> user() {
+        userService.flush("demo");
+        return userService.findById(1);
+    }
+
+    @RequestMapping("/appName")
+    public Mono<String> appName() {
+        userService.job1().doOnSuccess(s -> {
+        }).subscribe();
+        return userService.getAppName();
+    }
+
+    @RequestMapping("/flux")
+    public Mono<String> flux() {
+        userService.findAllPeople("vip").subscribe(t -> {
+            System.out.println(t.getNick());
+        });
+        return Mono.just("good");
+    }
+
+
+    @RequestMapping("/")
+    public String index() {
+        return "good";
+    }
+}
