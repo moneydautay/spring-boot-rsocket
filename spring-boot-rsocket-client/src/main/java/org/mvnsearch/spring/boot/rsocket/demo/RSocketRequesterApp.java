db2f2c68850f0e1b88fecf4588e9780dc586f80f
linux_china <libing.chen@gmail.com>
2018-09-19 11:37:35 -0700

    move subscribe operation to runner

--- spring-boot-rsocket-client/src/main/java/org/mvnsearch/spring/boot/rsocket/demo/RSocketRequesterApp.java
+++ spring-boot-rsocket-client/src/main/java/org/mvnsearch/spring/boot/rsocket/demo/RSocketRequesterApp.java
@@ -8,11 +8,8 @@ import io.rsocket.transport.netty.client.TcpClientTransport;
 import org.mvnsearch.spring.boot.rsocket.RSocketInvocationRequesterHandler;
 import org.mvnsearch.user.UserService;
 import org.reactivestreams.Publisher;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import reactor.core.publisher.Mono;
 
@@ -27,29 +24,10 @@ import java.util.function.Supplier;
  * @author linux_china
  */
 @SpringBootApplication
-public class RSocketRequesterApp implements ApplicationListener<ApplicationReadyEvent> {
-    @Autowired
-    RSocket rSocket;
-    @Autowired
-    UserService userService;
+public class RSocketRequesterApp {
 
     public static void main(String[] args) {
         SpringApplication.run(RSocketRequesterApp.class, args);
-    }
-
-    @Override
-    public void onApplicationEvent(ApplicationReadyEvent applicationReadyEvent) {
-      /*  rSocket
-                .requestStream(DefaultPayload.create("Hello"))
-                .map(Payload::getDataUtf8)
-                .doFinally(signal -> rSocket.dispose())
-                .subscribe(name -> System.out.println("consuming " + name + "."));*/
-        userService.findAllPeople("vip").subscribe(t -> {
-            System.out.println(t.getNick());
-        });
-        userService.findAllPeople("loser").subscribe(t -> {
-            System.out.println(t.getNick());
-        });
     }
 
     public Mono<RSocket> monoSocket() {
