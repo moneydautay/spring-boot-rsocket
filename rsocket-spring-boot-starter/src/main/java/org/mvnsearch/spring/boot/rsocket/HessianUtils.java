687b45390086a93a26848e14c8047dd1f3c6e1ba
linux_china <libing.chen@gmail.com>
2018-09-18 20:36:43 -0700

    hessian utils

---
+++ rsocket-spring-boot-starter/src/main/java/org/mvnsearch/spring/boot/rsocket/HessianUtils.java
@@ -0,0 +1,46 @@
+package org.mvnsearch.spring.boot.rsocket;
+
+import com.caucho.hessian.io.HessianSerializerInput;
+import com.caucho.hessian.io.HessianSerializerOutput;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.nio.ByteBuffer;
+
+/**
+ * hessian utils
+ *
+ * @author linux_china
+ */
+public class HessianUtils {
+
+    public static byte[] output(Object obj) {
+        try {
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            HessianSerializerOutput output = new HessianSerializerOutput(bos);
+            output.writeObject(obj);
+            output.flush();
+            return bos.toByteArray();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static Object input(byte[] content) {
+        try {
+            ByteArrayInputStream bis = new ByteArrayInputStream(content);
+            HessianSerializerInput input = new HessianSerializerInput(bis);
+            return input.readObject();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static Object input(ByteBuffer buffer) throws Exception {
+        HessianSerializerInput input = new HessianSerializerInput(new ByteBufferBackedInputStream(buffer));
+        return input.readObject();
+    }
+
+}
