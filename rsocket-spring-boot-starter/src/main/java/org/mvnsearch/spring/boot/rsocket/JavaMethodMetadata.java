ccb41bcaeff77347a4c916690a5b4033c87504f3
linux_china <libing.chen@gmail.com>
2018-09-20 10:26:00 -0700

    java method metadata

---
+++ rsocket-spring-boot-starter/src/main/java/org/mvnsearch/spring/boot/rsocket/JavaMethodMetadata.java
@@ -0,0 +1,117 @@
+package org.mvnsearch.spring.boot.rsocket;
+
+import org.mvnsearch.rsocket.RSocketProtos;
+import reactor.core.publisher.Flux;
+
+import java.lang.reflect.Method;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * java method metadata
+ *
+ * @author linux_china
+ */
+public class JavaMethodMetadata {
+    private static byte[] EMPTY_BODY = new byte[]{0};
+    private String classFullName;
+    private String name;
+    private int parameterCount;
+    private boolean isFluxReturnType = false;
+    private RSocketProtos.PayloadMetadata.Encoding encoding;
+
+    public JavaMethodMetadata(Method method) {
+        this.classFullName = method.getDeclaringClass().getCanonicalName();
+        this.name = method.getName();
+        //parameter
+        this.parameterCount = method.getParameterCount();
+        if (parameterCount == 0) {
+            encoding = RSocketProtos.PayloadMetadata.Encoding.VOID;
+        } else if (parameterCount == 1) {
+            Class<?> parameterType = method.getParameterTypes()[0];
+            if (parameterType.equals(Boolean.TYPE) || parameterType.equals(Boolean.class)) {
+                encoding = RSocketProtos.PayloadMetadata.Encoding.BOOL;
+            } else if (parameterType.equals(Byte.TYPE) || parameterType.equals(Byte.class)) {
+                encoding = RSocketProtos.PayloadMetadata.Encoding.BYTE;
+            } else if (parameterType.equals(Character.TYPE) || parameterType.equals(Character.class)) {
+                encoding = RSocketProtos.PayloadMetadata.Encoding.CHAR;
+            } else if (parameterType.equals(Short.TYPE) || parameterType.equals(Short.class)) {
+                encoding = RSocketProtos.PayloadMetadata.Encoding.SHORT;
+            } else if (parameterType.equals(Integer.TYPE) || parameterType.equals(Integer.class)) {
+                encoding = RSocketProtos.PayloadMetadata.Encoding.INT;
+            } else if (parameterType.equals(Long.TYPE) || parameterType.equals(Long.class)) {
+                encoding = RSocketProtos.PayloadMetadata.Encoding.LONG;
+            } else if (parameterType.equals(Float.TYPE) || parameterType.equals(Float.class)) {
+                encoding = RSocketProtos.PayloadMetadata.Encoding.FLOAT;
+            } else if (parameterType.equals(Double.TYPE) || parameterType.equals(Double.class)) {
+                encoding = RSocketProtos.PayloadMetadata.Encoding.DOUBLE;
+            } else if (parameterType.equals(String.class)) {
+                encoding = RSocketProtos.PayloadMetadata.Encoding.STRING;
+            }
+            //todo Date encoding
+        }
+        if (encoding == null) {
+            encoding = RSocketProtos.PayloadMetadata.Encoding.HESSIAN;
+        }
+        //return type
+        if (parameterCount == 1 && method.getParameterTypes()[0].equals(Flux.class)) {
+            isFluxReturnType = true;
+        }
+    }
+
+    public String getClassFullName() {
+        return classFullName;
+    }
+
+    public void setClassFullName(String classFullName) {
+        this.classFullName = classFullName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getParameterCount() {
+        return parameterCount;
+    }
+
+    public void setParameterCount(int parameterCount) {
+        this.parameterCount = parameterCount;
+    }
+
+    public RSocketProtos.PayloadMetadata.Encoding getEncoding() {
+        return encoding;
+    }
+
+    public void setEncoding(RSocketProtos.PayloadMetadata.Encoding encoding) {
+        this.encoding = encoding;
+    }
+
+    public boolean isFluxReturnType() {
+        return isFluxReturnType;
+    }
+
+    public void setFluxReturnType(boolean fluxReturnType) {
+        isFluxReturnType = fluxReturnType;
+    }
+
+    public ByteBuffer encodingBody(Object[] args) {
+        //todo no strategy pattern because of performance  and more encoding
+        if (this.encoding == RSocketProtos.PayloadMetadata.Encoding.VOID) {
+            return ByteBuffer.wrap(EMPTY_BODY);
+        } else if (this.encoding == RSocketProtos.PayloadMetadata.Encoding.INT) {
+            return ByteBuffer.allocate(4).putInt((int) args[0]);
+        } else if (this.encoding == RSocketProtos.PayloadMetadata.Encoding.LONG) {
+            return ByteBuffer.allocate(8).putLong((long) args[0]);
+        } else if (this.encoding == RSocketProtos.PayloadMetadata.Encoding.STRING) {
+            return ByteBuffer.wrap(((String) args[0]).getBytes(StandardCharsets.UTF_8));
+        } else { // encoding by hessian default
+            return ByteBuffer.wrap(HessianUtils.output(args));
+        }
+
+    }
+}
