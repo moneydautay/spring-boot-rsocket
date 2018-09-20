5e5d096d7c5a9fc0198ec48269818d7a286db654
linux_china <libing.chen@gmail.com>
2018-09-19 20:16:21 -0700

    polishing

--- rsocket-spring-boot-starter/src/main/java/org/mvnsearch/spring/boot/rsocket/RSocketResponderAutoConfiguration.java
+++ rsocket-spring-boot-starter/src/main/java/org/mvnsearch/spring/boot/rsocket/RSocketResponderAutoConfiguration.java
@@ -1,6 +1,5 @@
 package org.mvnsearch.spring.boot.rsocket;
 
-import io.rsocket.AbstractRSocket;
 import io.rsocket.RSocketFactory;
 import io.rsocket.SocketAcceptor;
 import io.rsocket.transport.netty.server.TcpServerTransport;
@@ -8,7 +7,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -29,20 +27,21 @@ public class RSocketResponderAutoConfiguration {
     RSocketProperties properties;
 
     @Bean
-    @ConditionalOnMissingBean(AbstractRSocket.class)
-    public RSocketRequestResponderHandler rSocketRequestResponderHandler() {
-        return new RSocketRequestResponderHandler();
-    }
-
-    @Bean
     public RSocketServiceAnnotationProcessor rSocketServiceAnnotationProcessor() {
         return new RSocketServiceAnnotationProcessor();
     }
 
+    /**
+     * rsocket responder, it's a Subscriber
+     *
+     * @param serviceCall service call
+     * @return Subscriber
+     */
     @Bean(destroyMethod = "dispose")
-    public Disposable rsocketResponder(AbstractRSocket myRSocket) {
-        SocketAcceptor socketAcceptor = (setupPayload, reactiveSocket) -> Mono.just(myRSocket);
-        Disposable rsocket = RSocketFactory
+    public Disposable rsocketResponder(ReactiveServiceCaller serviceCall) {
+        SocketAcceptor socketAcceptor = (setupPayload, reactiveSocket) -> Mono.just(new RSocketRequestResponderHandler(serviceCall, setupPayload));
+        //@see LambdaMonoSubscriber
+        Disposable subscriber = RSocketFactory
                 .receive()
                 .acceptor(socketAcceptor)
                 .transport(TcpServerTransport.create("0.0.0.0", properties.getPort()))
@@ -50,6 +49,6 @@ public class RSocketResponderAutoConfiguration {
                 .onTerminateDetach()
                 .subscribe();
         log.info("RSocket started on port " + properties.getPort());
-        return rsocket;
+        return subscriber;
     }
 }
