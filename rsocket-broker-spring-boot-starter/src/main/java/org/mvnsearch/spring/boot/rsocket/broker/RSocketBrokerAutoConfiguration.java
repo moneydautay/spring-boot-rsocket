e130e4229700055390bea9598a5b55c6c501c859
linux_china <libing.chen@gmail.com>
2018-10-01 11:34:16 -0700

    RSocket broker auto configuration

---
+++ rsocket-broker-spring-boot-starter/src/main/java/org/mvnsearch/spring/boot/rsocket/broker/RSocketBrokerAutoConfiguration.java
@@ -0,0 +1,20 @@
+package org.mvnsearch.spring.boot.rsocket.broker;
+
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * RSocket broker auto configuration
+ *
+ * @author linux_china
+ */
+@Configuration
+public class RSocketBrokerAutoConfiguration {
+
+
+    @PostConstruct
+    public void init() {
+
+    }
+}
