6b84facd1a9510b016f20cb5799786037e7df947
linux_china <libing.chen@gmail.com>
2018-10-03 14:51:16 -0700

    change UserService stub creation

--- spring-boot-rsocket-client/src/main/java/org/mvnsearch/spring/boot/rsocket/demo/RSocketRequesterApp.java
+++ spring-boot-rsocket-client/src/main/java/org/mvnsearch/spring/boot/rsocket/demo/RSocketRequesterApp.java
@@ -1,23 +1,14 @@
 package org.mvnsearch.spring.boot.rsocket.demo;
 
-import io.rsocket.AbstractRSocket;
 import io.rsocket.RSocket;
-import io.rsocket.RSocketFactory;
-import io.rsocket.client.LoadBalancedRSocketMono;
-import io.rsocket.client.filter.RSocketSupplier;
-import io.rsocket.transport.netty.client.TcpClientTransport;
-import org.mvnsearch.spring.boot.rsocket.RSocketInvocationRequesterHandler;
+import org.mvnsearch.spring.boot.rsocket.RSocketRemoteServiceBuilder;
 import org.mvnsearch.user.UserService;
-import org.reactivestreams.Publisher;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import reactor.core.publisher.Mono;
 
-import java.lang.reflect.Proxy;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.Map;
 
 /**
  * RSocket requester app
@@ -31,45 +22,12 @@ public class RSocketRequesterApp {
         SpringApplication.run(RSocketRequesterApp.class, args);
     }
 
-    @Bean()
-    public Mono<RSocket> rSocket() {
-        List<RSocketSupplier> suppliers = Stream.of("localhost:42252").map(url -> {
-            String[] parts = url.split(":");
-            return new RSocketSupplier(() -> Mono.just(rSocket(parts[0], Integer.valueOf(parts[1]))));
-        }).collect(Collectors.toList());
-        Publisher<List<RSocketSupplier>> src =
-                s -> {
-                    s.onNext(suppliers);
-                    s.onComplete();
-                };
-        return LoadBalancedRSocketMono.create(src);
-    }
-
     @Bean
-    public UserService userService(Mono<RSocket> rSocket) {
-        return (UserService) Proxy.newProxyInstance(
-                UserService.class.getClassLoader(),
-                new Class[]{UserService.class},
-                new RSocketInvocationRequesterHandler(rSocket, "application/hessian"));
+    public UserService userService(Map<String, Mono<RSocket>> rsockets) {
+        return (UserService) RSocketRemoteServiceBuilder
+                .client(UserService.class)
+                .endpoint("user-service")
+                .rSocket(rsockets)
+                .build();
     }
-
-    private RSocket rSocket(String host, int address) {
-        try {
-            return RSocketFactory
-                    .connect()
-                    .metadataMimeType("application/protobuf")
-                    .dataMimeType("application/hessian")
-                    .transport(TcpClientTransport.create(host, address))
-                    .start()
-                    .block();
-        } catch (Exception e) {
-            return new AbstractRSocket() {
-                @Override
-                public double availability() {
-                    return 0.0;
-                }
-            };
-        }
-    }
-
 }
