ef671917aabea0d0ef5f9c9dfae84309e82a1cf7
linux_china <libing.chen@gmail.com>
2018-10-03 15:26:35 -0700

    add Generic support

--- rsocket-spring-boot-starter/src/main/java/org/mvnsearch/spring/boot/rsocket/RSocketRemoteServiceBuilder.java
+++ rsocket-spring-boot-starter/src/main/java/org/mvnsearch/spring/boot/rsocket/RSocketRemoteServiceBuilder.java
@@ -1,6 +1,7 @@
 package org.mvnsearch.spring.boot.rsocket;
 
 import io.rsocket.RSocket;
+import org.springframework.lang.Nullable;
 import reactor.core.publisher.Mono;
 
 import java.lang.reflect.Proxy;
@@ -11,41 +12,42 @@ import java.util.Map;
  *
  * @author linux_china
  */
-public class RSocketRemoteServiceBuilder {
+public class RSocketRemoteServiceBuilder<T> {
+    @Nullable
     private String endpoint;
     private String service;
     private String version;
-    private Class serviceInterface;
+    private Class<T> serviceInterface;
     private Mono<RSocket> rSocket;
 
-    public static RSocketRemoteServiceBuilder client(Class serviceInterface) {
-        RSocketRemoteServiceBuilder rSocketServiceBuilder = new RSocketRemoteServiceBuilder();
+    public static <T> RSocketRemoteServiceBuilder<T> client(Class<T> serviceInterface) {
+        RSocketRemoteServiceBuilder<T> rSocketServiceBuilder = new RSocketRemoteServiceBuilder<T>();
         rSocketServiceBuilder.serviceInterface = serviceInterface;
         rSocketServiceBuilder.service = serviceInterface.getCanonicalName();
         return rSocketServiceBuilder;
     }
 
-    public RSocketRemoteServiceBuilder endpoint(String endpoint) {
+    public RSocketRemoteServiceBuilder<T> endpoint(String endpoint) {
         this.endpoint = endpoint;
         return this;
     }
 
-    public RSocketRemoteServiceBuilder service(String service) {
+    public RSocketRemoteServiceBuilder<T> service(String service) {
         this.service = service;
         return this;
     }
-    
-    public RSocketRemoteServiceBuilder version(String version) {
+
+    public RSocketRemoteServiceBuilder<T> version(String version) {
         this.version = version;
         return this;
     }
 
-    public RSocketRemoteServiceBuilder rSocket(Mono<RSocket> rSocket) {
+    public RSocketRemoteServiceBuilder<T> rSocket(Mono<RSocket> rSocket) {
         this.rSocket = rSocket;
         return this;
     }
 
-    public RSocketRemoteServiceBuilder rSocket(Map<String, Mono<RSocket>> rSockets) {
+    public RSocketRemoteServiceBuilder<T> rSocket(Map<String, Mono<RSocket>> rSockets) {
         if (rSockets.containsKey(service)) {
             this.rSocket = rSockets.get(service);
         } else {
@@ -54,10 +56,11 @@ public class RSocketRemoteServiceBuilder {
         return this;
     }
 
-    public Object build() {
-        return Proxy.newProxyInstance(
+    @SuppressWarnings("unchecked")
+    public T build() {
+        return (T) Proxy.newProxyInstance(
                 serviceInterface.getClassLoader(),
                 new Class[]{serviceInterface},
-                new RSocketInvocationRequesterHandler(rSocket, "application/hessian"));
+                new RSocketInvocationRequesterHandler(rSocket, "application/hessian", endpoint, serviceInterface, service, version));
     }
 }
