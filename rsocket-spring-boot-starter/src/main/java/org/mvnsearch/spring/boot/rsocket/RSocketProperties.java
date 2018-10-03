c7c6aa010fe0aecfaebd94889b5a5a449c1067be
linux_china <libing.chen@gmail.com>
2018-10-02 20:27:43 -0700

    add broker & endpoints

--- rsocket-spring-boot-starter/src/main/java/org/mvnsearch/spring/boot/rsocket/RSocketProperties.java
+++ rsocket-spring-boot-starter/src/main/java/org/mvnsearch/spring/boot/rsocket/RSocketProperties.java
@@ -2,6 +2,8 @@ package org.mvnsearch.spring.boot.rsocket;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.util.Map;
+
 /**
  * RSocket Properties
  *
@@ -15,6 +17,14 @@ public class RSocketProperties {
      * listen port, default is 42252
      */
     private Integer port = 42252;
+    /**
+     * broker url, such tcp://127.0.0.1:42252
+     */
+    private String broker;
+    /**
+     * endpoints: interface full name to endpoint url
+     */
+    private Map<String, String> endpoints;
 
     public Integer getPort() {
         return port;
@@ -22,5 +32,21 @@ public class RSocketProperties {
 
     public void setPort(Integer port) {
         this.port = port;
+    }
+
+    public String getBroker() {
+        return broker;
+    }
+
+    public void setBroker(String broker) {
+        this.broker = broker;
+    }
+
+    public Map<String, String> getEndpoints() {
+        return endpoints;
+    }
+
+    public void setEndpoints(Map<String, String> endpoints) {
+        this.endpoints = endpoints;
     }
 }
